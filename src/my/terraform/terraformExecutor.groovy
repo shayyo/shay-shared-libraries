5088a9e892a73668e1085bc7c7f457189432ee49
shayyo <shay.yosef@gmail.com>
2023-12-03 22:46:43 +0200

    Update terraformExecutor.groovy

--- src/my/terraform/terraformExecutor.groovy
+++ src/my/terraform/terraformExecutor.groovy
@@ -1,1 +1,17 @@
-s
+package my.terraform
+
+def initialize() {
+    sh 'echo "terraform init"'
+}
+
+def plan() {
+    // Code to create a Terraform plan
+}
+
+def apply() {
+    // Code to apply Terraform changes
+}
+
+def destroy() {
+    // Code to destroy Terraform resources
+}
