3d0cddc41d7179175ede82e6c750318257e35495
shayyo <shay.yosef@gmail.com>
2023-12-04 10:41:57 +0200

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
-def call(Map config = [:]) {
-  def scriptcontents = libraryResource "scripts/linux/${config.name}"
-  writeFile file = "${config.name}", text: scriptcontents
-  sh "chmod a+x ./${config.name}"
+def call(scriptname) {
+  def scriptcontents = libraryResource "scripts/linux/${scriptname}"
+  writeFile file = "${scriptname}", text: scriptcontents
+  sh "chmod a+x ./${scriptname}"
 }
