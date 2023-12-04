63a208aa6710155878de0b769f0e5fa7e700bf5b
shayyo <shay.yosef@gmail.com>
2023-12-04 12:48:52 +0200

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(String scriptname) {
-  def scriptcontents = libraryResource "scripts/linux/hello_world.sh}"
+  def scriptcontents = libraryResource "scripts/linux/hello_world.sh"
   writeFile file = "hello_world.sh", text: scriptcontents
 
 }
