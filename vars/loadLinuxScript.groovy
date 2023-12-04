f0d919a2b63ddc19eb679c32225316f3e80ea49c
shayyo <shay.yosef@gmail.com>
2023-12-04 15:10:54 +0200

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(String scriptname) {
   def scriptcontents = libraryResource "scripts/linux/${scriptname}"
   writeFile file: "${scriptname}", text: scriptcontents
-  sh "chmod a+x ./${scriptname}"
+  
 }
