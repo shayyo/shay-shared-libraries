0712bd2a45e07a798fed33e602099a71b2e68840
shayyo <shay.yosef@gmail.com>
2023-12-05 16:12:54 +0200

    Update runCommandOnLinux.groovy

--- vars/runCommandOnLinux.groovy
+++ vars/runCommandOnLinux.groovy
@@ -1,5 +1,3 @@
 def call(String host, String port, String username, String password, String command) {
-    // Use SSH to run the arbitrary command on the Linux machine
     sshpass -p ${password} ssh -o StrictHostKeyChecking=no ${username}@${host} -p ${port} ${command}
-
 }
