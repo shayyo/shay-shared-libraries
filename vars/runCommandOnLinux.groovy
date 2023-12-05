4bce4ee77e1551637577a502ff8988e6b64d3708
shayyo <shay.yosef@gmail.com>
2023-12-05 15:59:05 +0200

    Update runCommandOnLinux.groovy

--- vars/runCommandOnLinux.groovy
+++ vars/runCommandOnLinux.groovy
@@ -1,23 +1,6 @@
-def call(String host, String username, String password, String command) {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Run Command on Linux Machine') {
-                steps {
-                    script {
-                        def output = runCommand(host, username, password, command)
-                        echo "Command output: ${output}"
-                    }
-                }
-            }
-        }
-    }
-}
-
-def runCommand(String host, String username, String password, String command) {
+def call(String host, String port, String username, String password, String command) {
     // Use SSH to run the arbitrary command on the Linux machine
-    def cmd = "sshpass -p ${password} ssh ${username}@${host} '${command}'"
+    def cmd = "sshpass -p ${password} ssh -o StrictHostKeyChecking=no ${username}@${host} -p ${port} '${command}'"
     def process = cmd.execute()
     process.waitFor()
 
