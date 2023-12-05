8693d07e3801846edfd9cbaa2a1d19a8e5856ee6
shayyo <shay.yosef@gmail.com>
2023-12-05 16:11:07 +0200

    Update runCommandOnLinux.groovy

--- vars/runCommandOnLinux.groovy
+++ vars/runCommandOnLinux.groovy
@@ -1,8 +1,5 @@
 def call(String host, String port, String username, String password, String command) {
     // Use SSH to run the arbitrary command on the Linux machine
-    def cmd = "sshpass -p ${password} ssh -o StrictHostKeyChecking=no ${username}@${host} -p ${port} '${command}'"
-    def process = cmd.execute()
-    process.waitFor()
+    sshpass -p ${password} ssh -o StrictHostKeyChecking=no ${username}@${host} -p ${port} ${command}
 
-    return process.text.trim()
 }
