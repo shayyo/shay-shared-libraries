3c69b7c125e56527f4b2d55000d8c330adb2f4de
shayyo <shay.yosef@gmail.com>
2023-12-06 10:47:22 +0200

    Update loadTerraformEc2Config.groovy

--- vars/loadTerraformEc2Config.groovy
+++ vars/loadTerraformEc2Config.groovy
@@ -1,17 +1,3 @@
 def call(String region) {
-echo '
-  provider \"aws\" {
-    region = \"eu-central-1\"
-  }
-
-  resource \"aws_instance\" \"enforcer\" {
-    ami           = \"ami-02a015c2b1c34f1a8\"
-    instance_type = \"t2.medium\"
-    count 	= 5
-    key_name	= \"CNDR_key\"
-
-    tags = {
-      Name = \"enforcer\"
-    }
-  }'
+  sh "echo 'DDDD'"
 }
