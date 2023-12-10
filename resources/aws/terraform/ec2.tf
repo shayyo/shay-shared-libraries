3fb06f3986148659a6d1c9b4886220917b5036cc
shayyo <shay.yosef@gmail.com>
2023-12-10 10:20:05 +0200

    Update ec2.tf

--- resources/aws/terraform/ec2.tf
+++ resources/aws/terraform/ec2.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = "eu-central-1"
+}
 
+resource "aws_instance" "enforcer" {
+  ami           = "ami-02a015c2b1c34f1a8"
+  instance_type = "t2.medium"
+  count 	= 5
+  key_name	= "my_key_name"
+
+  tags = {
+    Name = "my EC2"
+  }
+}
