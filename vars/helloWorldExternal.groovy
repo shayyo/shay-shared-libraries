e0f5433110ad407ec061046a88ace6d936deb31d
shayyo <shay.yosef@gmail.com>
2023-12-13 16:28:43 +0200

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
   loadLinuxScript("hello_world.sh")
-  sh "./hello_world.sh ${config.lname} ${config.fname}"
+  sh "./hello_world.sh ${config.fname} ${config.lname}"
 }
